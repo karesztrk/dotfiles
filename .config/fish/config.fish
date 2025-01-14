189e095326d1d2739d7accb1e1cfd0524ff18970
Károly Török <torok.karoly.krisztian@gmail.com>
2025-01-14 11:19:24 +0100

    Switch to lla

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -105,11 +105,11 @@ end
 
 ## Useful aliases
 # Replace ls with eza
-alias ls 'eza -al --color=always --group-directories-first --icons' # preferred listing
-alias la 'eza -a --color=always --group-directories-first --icons' # all files and dirs
-alias ll 'eza -l --color=always --group-directories-first --icons' # long format
-alias lt 'eza -aT --color=always --group-directories-first --icons' # tree listing
-alias l. 'eza -ald --color=always --group-directories-first --icons .*' # show only dotfiles
+alias ls 'lla -l' # preferred listing
+alias lg 'lla -G' # git format
+alias lm 'lla -t' # timeline format
+alias lt 'lla -t' # tree listing
+alias l. 'lla --dotfiles-only' # show only dotfiles
 
 # Replace some more things with better alternatives
 alias cat='bat --style header --style snip --style changes --style header'
@@ -138,7 +138,7 @@ alias egrep='egrep --color=auto'
 alias hw='hwinfo --short' # Hardware Info
 alias big="expac -H M '%m\t%n' | sort -h | nl" # Sort installed packages according to size in MB
 alias gitpkg='pacman -Q | grep -i "\-git" | wc -l' # List amount of -git packages
-alias usage='du -h -d 1'
+alias usage='lla -S --include-dirs'
 
 # Get fastest mirrors
 alias mirror="sudo reflector -f 30 -l 30 --number 10 --verbose --save /etc/pacman.d/mirrorlist"
