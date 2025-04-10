a2e53d05475ab4b85899deb102675803c1d19830
Károly Török <torok.karoly.krisztian@gmail.com>
2025-04-10 11:43:22 +0200

    Carapace completion update after Fish v4.0.1

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -42,8 +42,6 @@ end
 
 ## Carapace autocompletion
 set -Ux CARAPACE_BRIDGES 'zsh,fish,bash,inshellisense' # optional
-mkdir -p ~/.config/fish/completions
-carapace --list | awk '{print $1}' | xargs -I{} touch ~/.config/fish/completions/{}.fish # disable auto-loaded completions (#185)
 carapace _carapace | source
 
 ## Advanced command-not-found hook
